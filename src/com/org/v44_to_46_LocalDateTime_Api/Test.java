e77112604955b21f94c54e28f82d92fd41215e70
iamsubho76 <iamsubho76@gmail.com>
2017-08-03 13:56:45 +0530

    Added Code for Local Date Time Api.

---
+++ src/com/org/v44_to_46_LocalDateTime_Api/Test.java
@@ -0,0 +1,19 @@
+package com.org.v44_to_46_LocalDateTime_Api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		List<String> list = getList();
+		List<String> listOpt = list != null ? list : new ArrayList<>();
+
+	}
+
+	private static List<String> getList() {
+		return null;
+	}
+
+}
